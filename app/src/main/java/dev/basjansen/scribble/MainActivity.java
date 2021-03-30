f57ce308c0f75a482d73d81553eb9242327009c2
Bastiaan Jansen <Bastiaanj7@outlook.com>
2021-03-30 14:24:37 +0200

    my drawings view

--- app/src/main/java/dev/basjansen/scribble/MainActivity.java
+++ app/src/main/java/dev/basjansen/scribble/MainActivity.java
@@ -1,27 +1,16 @@
 package dev.basjansen.scribble;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.NavController;
-import androidx.navigation.NavHostController;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
 
-import com.google.android.material.bottomappbar.BottomAppBar;
-import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.HashMap;
 import java.util.Map;
-
-import dev.basjansen.scribble.models.Drawing;
-import dev.basjansen.scribble.services.DrawingService;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -33,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
 
         Map<Integer, Fragment> pagesMap = new HashMap<>();
         pagesMap.put(R.id.gallery, new GalleryFragment());
-        pagesMap.put(R.id.my_drawings, new MyDrawingsFragment());
+        pagesMap.put(R.id.my_drawings_reclycler_view, new MyDrawingsFragment());
         pagesMap.put(R.id.settings, new SettingsFragment());
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
--- app/src/main/java/dev/basjansen/scribble/MyDrawingsFragment.java
+++ app/src/main/java/dev/basjansen/scribble/MyDrawingsFragment.java
@@ -3,22 +3,52 @@ package dev.basjansen.scribble;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import dev.basjansen.scribble.models.Drawing;
+import dev.basjansen.scribble.services.DrawingService;
+
 public class MyDrawingsFragment extends Fragment {
+
+    private DrawingService drawingService;
+    private DrawingsAdapter drawingsAdapter;
 
     public MyDrawingsFragment() {}
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        drawingService = new DrawingService();
+        drawingsAdapter = new DrawingsAdapter(getContext());
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_my_drawings, container, false);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        setupDrawingsList();
+
+        drawingService.fetch((Drawing[] drawings) -> {
+            drawingsAdapter.setDrawings(drawings);
+            drawingsAdapter.notifyDataSetChanged();
+        }, Exception::printStackTrace);
+    }
+
+    private void setupDrawingsList() {
+        RecyclerView drawingsRecycleView = getView().findViewById(R.id.my_drawings_reclycler_view);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        drawingsRecycleView.setLayoutManager(linearLayoutManager);
+        drawingsRecycleView.setAdapter(drawingsAdapter);
     }
 }
